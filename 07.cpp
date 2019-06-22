6596f62cd3c03bbf9b3bc8fd37868c8fa949ab44
Dilpreet Singh Banga <43845241+dprt69@users.noreply.github.com>
2019-06-22 15:03:23 +0530

    Add files via upload

---
+++ 07.cpp
@@ -0,0 +1,77 @@
+//
+//  07.cpp
+//  06
+//
+//  Created by Dilpreet on 22/06/19.
+//  Copyright © 2019 Dilpreet. All rights reserved.
+//
+// WAP to perform nested loop activity.
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{   int i{0};
+    int num[5];
+    char ch='0';
+    int large,small;
+    //int v{};
+    cout<<"Input the first 4 no.s : ";
+    cin>>num[0];
+    cin>>num[1];
+    cin>>num[2];
+    cin>>num[3];
+    cout<<"You have stored these no.s : ";
+    cout<<num[0]<<" "<<num[1]<<" "<<num[2]<<" "<<num[3]<<endl;
+    cout<<"P-Print numbers\n";
+    cout<<"A-Add a number\n";
+    cout<<"M-Display mean of the number\n";
+    cout<<"S-Display the smallest number\n";
+    cout<<"L-Display the largest number\n";
+    cout<<"Q-Quit\n";
+    cout<<"--------------------------------\n";
+    cout<<"Enter any initial mentioned above :- ";
+    cin>>ch;
+    {
+        if (ch=='p'|| ch=='P') {
+            cout<<"P-Print numbers : ";
+            cout<<num[0]<<" "<<num[1]<<" "<<num[2]<<" "<<num[3]<<" "<<"\n";
+        }
+        if(ch=='a'|| ch=='A'){
+            cout<<"A-Add a number : ";
+            cin>>num[4];
+            cout<<num[4]<<endl;
+            cout<<"Now the nos stored are : ";
+            cout<<num[0]<<" "<<num[1]<<" "<<num[2]<<" "<<num[3]<<" "<<num[4]<<" "<<endl;
+        }
+        if (ch=='m'||ch=='M') {
+            cout<<"M-Display mean of the number : ";
+            cout<<((num[0]+num[1]+num[2]+num[3])/4);
+        }
+        if (ch=='s'||ch=='S') {
+            large=small=num[i];
+            for (i=0; i<5; ++i) {
+                if (small>num[i]) {
+                    small=num[i];
+                    cout<<"S-Display the smallest number : "<<small<<endl;
+                }
+            }
+        }
+        if (ch=='l'||ch=='L') {
+            large=small=num[0];
+            for (i=0; i<5; ++i) {
+                if (large<num[i]) {
+                    large=num[i];
+                    cout<<"L-Display the largest number\n"<<large<<endl;
+                }
+            }
+        }
+        if (ch=='q'||ch=='Q') {
+            cout<<"Q-Quit\n";
+        }
+    }
+    
+    // if(ch!='p'|| ch!='P'||ch!='a'|| ch!='A' ||ch!='m'||ch!='M'||ch!='s'||ch!='S'||ch!='l'||ch!='L'||ch!='q'||ch!='Q'){
+    //cout<<"Unknown selection, please try again.\n";
+    //}
+    return 0;
+}
